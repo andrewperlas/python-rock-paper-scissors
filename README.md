6ddc7660ffdba6d704fc7b67147aa980e853e5c2
Andrew <andrew@andrewperlas.com>
2024-06-11 18:52:39 -1000

    create rock-paper-scissors python program

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # python-rock-paper-scissors
+
+## Description
+It's a rock-paper-scissors game against the computer
+
+## Program Logic
+- Start with user and computer wins as 0
+- Store rock/paper/scissors selections in an array
+- Ask user to select rock, paper, or scissors
+- Compare user's selection with the computer's randomly generated selection to see who wins
+- Add to count of user or computer wins
+
+## Learnings
---
+++ rock-paper-scissors.py
@@ -0,0 +1,36 @@
+import random
+
+user_wins = 0
+computer_wins = 0
+
+selection = ["rock", "paper", "scissors"]
+
+while True:
+    user_input = input("Type Rock/Paper/Scissors or Q to quit").lower()
+    if user_input == "q":
+        break
+    
+    if user_input not in selection:
+        print("Invalid option")
+        continue
+
+    random_number = random.randint(0, 2)
+    # rock: 0, paper: 1, scissors: 2
+
+    computer_selection = selection[random_number]
+    print(f"Computer selected {computer_selection}")
+
+    if user_input == "rock" and computer_selection == "scissors":
+        print("You won!")
+        user_wins += 1
+    elif user_input == "paper" and computer_selection == "rock":
+        print("You won!")
+        user_wins += 1
+    elif user_input == "scissors" and computer_selection == "paper":
+        print("You won!")
+        user_wins += 1
+    else:
+        print("You lost!")
+        computer_wins += 1
+
+print("Game Over")
